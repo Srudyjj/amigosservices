b929b54e934427d02528d1cd95b565073e0d6428
Serhii <srudyjj@gmail.com>
2022-07-06 17:37:25 +0300

    Fix Notification service

--- notification/src/main/java/com/amigoscode/notification/NotificationController.java
+++ notification/src/main/java/com/amigoscode/notification/NotificationController.java
@@ -2,6 +2,7 @@ package com.amigoscode.notification;
 
 import com.amigoscode.clients.notification.NotificationRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class NotificationController {
         this.service = service;
     }
 
+    @PostMapping
     public void notify(@RequestBody NotificationRequest request) {
         log.info("notification {}", request);
         service.notify(request);
